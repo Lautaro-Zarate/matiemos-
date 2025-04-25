5425e180d35319e6e203f337cefca668287bfd4e
Lautaro Zarate <zlautaro43@gmail.com>
2025-04-25 13:43:28 -0300

    Agregado de logica en componente DrinkCard y SelectorDrinkContainer

--- components/DrinkCard.jsx
+++ components/DrinkCard.jsx
@@ -1,8 +1,8 @@
 const DrinkCard = (props) => {
-    const {image, name} = props;
+    const {image, name, isSelected, onClick} = props;
     return(
         <div>
-            <div className="drink-card">
+            <div className={`drink-card ${isSelected ? "selected" : ""}`} onClick={onClick}>
                 <img src={image} alt={name}/>
                 <h2>{name}</h2>
             </div>
--- components/SelectorDrinkContainer.jsx
+++ components/SelectorDrinkContainer.jsx
@@ -1,10 +1,32 @@
+import { useReducer } from "react";
 import DrinkCard from "./DrinkCard"
+
+const reducer = (state, action) => {
+    return state === action.index ? null : action.index;
+}
+
 const SelectorDrinkContainer = () => {
+    const [selectedDrink, dispatch] = useReducer(reducer, null);
+
+    const drinks = [
+        {image: "/public/isotipo.png", name: "Mate"},
+        {image: "/public/cafe-card.png", name: "Café"},
+        {image: "/public/te-card.png", name: "Té"}
+    ]
+
     return(
         <div className="selector-drink-container">
-            <DrinkCard image={"/public/isotipo.png"} name={"Mate"}/>
-            <DrinkCard image={"/public/cafe-card.png"} name={"Café"}/>
-            <DrinkCard image={"/public/te-card.png"} name={"Té"}/>
+            {
+                drinks.map((drink, index) => (
+                    <DrinkCard
+                    key={index}
+                    image={drink.image}
+                    name={drink.name}
+                    isSelected={selectedDrink === index}
+                    onClick={() => dispatch({index})}
+                    />
+                ))
+            }
         </div>
     )
 }
